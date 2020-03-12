5f5e8cd25b68423c3acdcc52e95a8add69d35e0a
Maria Casby <mariaallen@comcast.net>
2020-03-12 17:13:00 -0400

    trying to fix storage problem

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ $(document).ready(function(){
 var myChores = localStorage.getItem('chores')
  var myInd = localStorage.getItem('ind');
  //splitting the long strings from local storage into arrays of indiv chores and textarea input numbers.
+ if (splitInd !== null) {
   var splitInd = myInd.split(",")
   var splitChores = myChores.split(",")
-
+}
 //linking chores to corresponding text areas.
 var finalNum;
   for (var i = 0; i < splitInd.length; i++) {
